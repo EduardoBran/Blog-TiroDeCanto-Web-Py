80a88b380a2134c5222be351bd2f43a588288841
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-25 15:51:37 -0300

    criado models e admin para postsapp

--- postsapp/admin.py
+++ postsapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo_post', 'autor_post', 'data_post',
+                    'categoria_post', 'publicado_post',)
+    list_editable = ('publicado_post',)
+    list_display_links = ('id', 'titulo_post',)
+
+
+admin.site.register(Post, PostAdmin)
--- postsapp/models.py
+++ postsapp/models.py
@@ -1,3 +1,16 @@
+from categorias.models import Categoria
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    titulo_post = models.CharField(max_length=100, verbose_name='Título')
+    autor_post = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Autor')
+    data_post = models.DateTimeField(default=timezone.now, verbose_name='Data')
+    conteudo_post = models.TextField(verbose_name='Conteúdo')
+    excerto_post = models.TextField(verbose_name='Excerto')
+    categoria_post = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, blank=True, null=True)
+    imagem_post = models.URLField(blank=True, null=True)
+    publicado_post = models.BooleanField(default=False)
+    
