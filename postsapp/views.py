1501b539a9e728939fbe71b17fce613a2930c5ce
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-25 17:12:42 -0300

    add condicao para exibir posts como publicado

--- postsapp/views.py
+++ postsapp/views.py
@@ -15,7 +15,7 @@ class PostIndex(ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         
-        qs = qs.order_by('data_post')
+        qs = qs.order_by('data_post').filter(publicado_post=True)
         qs = qs.annotate(
             numero_comentarios=Count(
                 Case(
