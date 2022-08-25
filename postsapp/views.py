58bb1be73240a2c7f439fd4e0b84b38d00060c9d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-25 17:05:10 -0300

    exibindo os posts cadastrados

--- postsapp/views.py
+++ postsapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 
+from postsapp.models import Post
 
-class PostIndex(TemplateView):
+
+class PostIndex(ListView):
+    model = Post
     template_name = 'postsapp/index.html'
-
+    paginate_by = 6
+    context_object_name = 'posts'
+    
+        
     
