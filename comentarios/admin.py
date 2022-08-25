91aa877bcf019d11b98825cd8ab70773dd900e3d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-25 16:21:24 -0300

    criado models e admin para comentarios

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comentario
+
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_comentario', 'email_comentario', 'post_comentario',
+                    'data_comentario', 'publicacao_comentario',)
+    list_editable = ('publicacao_comentario',)
+    list_display_links = ('id', 'nome_comentario', 'email_comentario',)
+
+
+admin.site.register(Comentario, ComentarioAdmin)
--- comentarios/models.py
+++ comentarios/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+from postsapp.models import Post
 
-# Create your models here.
+
+class Comentario(models.Model):
+    nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
+    email_comentario = models.EmailField(verbose_name='E-mail')
+    comentario = models.TextField(verbose_name='Comentário')
+    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post')
+    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Usuário')
+    data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Data')
+    publicacao_comentario = models.BooleanField(default=False, verbose_name='Publicar')
