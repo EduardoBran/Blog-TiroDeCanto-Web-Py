316341ffe3bc1e33ccd772432386f73606834cc3
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-26 15:20:41 -0300

    edit models comentarios

--- comentarios/models.py
+++ comentarios/models.py
@@ -9,6 +9,6 @@ class Comentario(models.Model):
     email_comentario = models.EmailField(verbose_name='E-mail')
     comentario = models.TextField(verbose_name='Comentário')
     post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post')
-    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Usuário')
+    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Usuário', blank=True, null=True)
     data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Data')
     publicacao_comentario = models.BooleanField(default=False, verbose_name='Publicar')
