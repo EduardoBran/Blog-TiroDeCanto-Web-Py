1fd9fcc0ff15c61439531856abb497698b96aaf3
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-29 15:19:15 -0300

    add opcao envio de email | edit layout page contato

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from categorias.models import Categoria
+from django.contrib import messages
 from django.shortcuts import render
+
+from .forms import ContatoForm
 
 
 def sobreView(request):
@@ -11,4 +14,21 @@ def sobreView(request):
 
 
 def contatoView(request):
-    return render(request, 'main/contato.html')
+    
+    if str(request.method) == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            messages.success(request, 'E-mail enviado com sucesso.')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'E-mail NÃO foi enviado.')
+    else:
+        form = ContatoForm()
+    
+    categorias =  Categoria.objects.order_by('nome_cat')
+    context = {
+        'form': form,
+        'categorias': categorias
+    }
+    return render(request, 'main/contato.html', context)
